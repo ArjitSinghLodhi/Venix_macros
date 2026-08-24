4862467645b9dd438a4752ad41d3efabb27afe1b
ArjitSinghLodhi <arjitsinghlodhi31@gmail.com>
2026-08-24 21:38:35 +0530

    added .gitignore

--- target/debug/build/quote-6dff9724e4e81362/build_script_build-6dff9724e4e81362.d
+++
@@ -1,5 +0,0 @@
-/home/arjit/venix_derive/target/debug/build/quote-6dff9724e4e81362/build_script_build-6dff9724e4e81362.d: /home/arjit/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.47/build.rs
-
-/home/arjit/venix_derive/target/debug/build/quote-6dff9724e4e81362/build_script_build-6dff9724e4e81362: /home/arjit/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.47/build.rs
-
-/home/arjit/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.47/build.rs:
--- target/debug/deps/venix_derive-29cbc4cb30e27eb2.d
+++
@@ -1,9 +0,0 @@
-/home/arjit/venix_derive/target/debug/deps/venix_derive-29cbc4cb30e27eb2.d: src/lib.rs src/macros/bundle.rs src/macros/filter.rs src/macros/system_param.rs src/macros/query_data.rs
-
-/home/arjit/venix_derive/target/debug/deps/libvenix_derive-29cbc4cb30e27eb2.rmeta: src/lib.rs src/macros/bundle.rs src/macros/filter.rs src/macros/system_param.rs src/macros/query_data.rs
-
-src/lib.rs:
-src/macros/bundle.rs:
-src/macros/filter.rs:
-src/macros/system_param.rs:
-src/macros/query_data.rs:
